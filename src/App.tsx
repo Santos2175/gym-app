d8a8a5c06f676d8fbd4b15501e2606604ac717a6
Santos2175 <santoshgurung949@gmail.com>
2025-05-06 04:57:26 +0545

    feat: navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Navbar from '@/components/navbar';
+import { SelectedPage } from '@/types/types';
+import { useState } from 'react';
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
   return (
     <>
-      <h1 className='app bg-gray-20'>app</h1>
+      <div className='app bg-gray-20'>
+        <Navbar
+          isTopOfPage={isTopOfPage}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      </div>
     </>
   );
 }
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import Logo from '@/assets/Logo.png';
+import Link from './Link';
+import { SelectedPage } from '@/types/types';
+import ActionButton from '@/components/ui/ActionButton';
+
+type Props = {
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = 'flex items-center justify-between';
+  const navbarBackground = isTopOfPage ? '' : 'bg-primary-100 drop-shadow';
+
+  return (
+    <nav>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 w-full z-30 py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img src={Logo} alt='logo' />
+
+            {/* RIGHT SIDE */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page='Home'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page='Benefits'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page='Our Classes'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page='Contact Us'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Become a member
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
