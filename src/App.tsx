d195649e51d43277039e8b09dab584d9ea862c10
Santos2175 <santoshgurung949@gmail.com>
2025-05-06 08:39:39 +0545

    fix:navbar modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from '@/components/navbar';
 import { SelectedPage } from '@/types/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Home from './components/home';
 
 function App() {
@@ -8,6 +8,24 @@ function App() {
     SelectedPage.Home
   );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <div className='app bg-gray-20'>
